618eb535189f4be38687e27c19adf9f53d2ad55b
Arthur dos Santos Lima <83731694+Fastusx@users.noreply.github.com>
2026-07-07 04:28:40 -0300

    Add initial game engine setup in script.js

---
+++ P2/js/script.js
@@ -0,0 +1,8 @@
+import { MotorDeJogoFacade } from './facade.js';
+
+
+const engine = new MotorDeJogoFacade();
+
+window.facade = engine;
+
+console.log("Motor de combate RPG inicializado com sucesso!");
